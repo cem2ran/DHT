d35644968ef5aece997cb04b76f2f5c153e328f7
KlausKlokmose <klausklokmose@gmail.com>
2015-11-18 17:51:36 +0100

    added some more

--- src/dk/itu/modis/Peer.java
+++ src/dk/itu/modis/Peer.java
@@ -1,5 +1,3 @@
-package dk.itu.modis;
-
 import javafx.util.Pair;
 
 import java.io.IOException;
@@ -7,13 +5,11 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Peer {
-
+//HELLO
     private final Map<Integer, String> values = new HashMap<>();
     public final Map<UUID, String> routingTable;
 
@@ -31,13 +27,33 @@ public class Peer {
     }
 
     public static void main(String[] args) {
+    //TODO add input from args
+        String myhost = "localhost";
 
-        try {
-            Peer me = join("localhost", 42000, "localhost", 1025);
+        if(args.length == 1){
+            //boot peer
+            Peer me = new Peer(UUID.randomUUID(), "localhost", Integer.parseInt(args[0]), new ConcurrentHashMap<>() );
+            me.routingTable.put(me.id, me.host+":"+me.port);
             me.receive();
+        }else if(args.length > 1){
+            int myPort = 0;
+            String hostIP = null;
+            int hostPort = 0;
+            try {
+                myPort = Integer.parseInt(args[0]);
+                hostIP = args[1];
+                hostPort = Integer.parseInt(args[2]);
+            } catch (NumberFormatException e) {
+                System.out.println("not enough args!");
+            }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            try {
+                Peer me = join(myhost, myPort, hostIP, hostPort);
+                me.receive();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
 
@@ -55,29 +71,49 @@ public class Peer {
                             ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
                             Object o = in.readObject();
 
+                            String connectedHost = socket.getRemoteSocketAddress().toString();
+                            int connectedPort = socket.getPort();
+
                             if(o instanceof String){
                                 String[] msg = o.toString().split(":");
+                                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+
                                 switch (msg[0]){
+                                    case JOIN_MSG:
+
+                                        //replyWithNodesUUIDAndRoutingTable
+                                        out.writeObject(routingTable); //return a copy of routing table
+                                        out.close();
+                                        break;
+
                                     case LEAVE_MSG:
                                         removeHostFromRoutingTable(msg[1]);
                                         break;
-                                    case JOIN_MSG:
-                                        String host = socket.getRemoteSocketAddress().toString();
-                                        int port = socket.getPort();
-                                        // Generate at client instead
-                                        //UUID nodeID = UUID.randomUUID(); //TODO GUID?
 
-                                        //String UUID = msg[1];
-                                        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-                                        out.writeObject(routingTable);
-
-
-
-                                        //replyWithNodesUUIDAndRoutingTable
-
-                                        break;
                                     case CLOSEST_MSG:
+                                        UUID target = UUID.fromString(msg[1]);
+                                        //replyWithNodesUUIDAndRoutingTable
+                                        UUID[] sorted = getSortedRoutingTableByDistanceToTarget(target);
+                                        UUID closest = sorted[0];
+                                        //return a copy of routing table entry
+                                        out.writeObject(new Pair<>(closest, routingTable.get(closest)));
+                                        out.close();
 
+                                        //find out if we should update our own table
+                                        final UUID meAsTarget = id;
+                                        UUID[] sortedByMe = getSortedRoutingTableByDistanceToTarget(meAsTarget);
+
+                                        //add those peers that we haven't met before
+                                        for (int i = 0; i < 4; i++) {
+                                            if(!routingTable.containsKey(sortedByMe[i])){
+                                                routingTable.put(sortedByMe[i], connectedHost+":"+connectedPort);
+                                            }
+                                        }
+
+                                        //remove those peers that are further than 4 peers away
+                                        for (int i = 4; i < sortedByMe.length; i++) {
+                                            routingTable.remove(sortedByMe[i]);
+                                        }
                                         break;
                                 }
                             }else{
@@ -88,6 +124,7 @@ public class Peer {
                             e.printStackTrace();
                         } catch (ClassNotFoundException e) {
                             e.printStackTrace();
+                        }finally {
                         }
                     }
                 }).start();
@@ -96,6 +133,13 @@ public class Peer {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private UUID[] getSortedRoutingTableByDistanceToTarget(UUID target) {
+        //sort list
+        UUID[] sorted = routingTable.keySet().toArray(new UUID[routingTable.size()]);
+        Arrays.sort(sorted, (o1, o2) -> distance(target, o1)  - distance(target, o2));
+        return sorted;
     }
 
 
@@ -108,25 +152,36 @@ public class Peer {
             Socket client = new Socket(myHost, myPort);
 
             ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
-            UUID id = UUID.randomUUID();
-            out.write((JOIN_MSG+":"+id.toString()).getBytes("UTF-8"));
+            //TODO should maybe be done by boot peer
+            UUID uuid = UUID.randomUUID();
+            out.write((JOIN_MSG+":"+uuid.toString()).getBytes("UTF-8"));
 
             ObjectInputStream in = new ObjectInputStream(client.getInputStream());
 
-            Map<UUID, String> bootNodeRoutingTable = (Map<UUID, String>) in.readObject();
+            Map<UUID, String> bootNodeRoutingTable = (ConcurrentHashMap<UUID, String>) in.readObject(); //blocks
 
             assert bootNodeRoutingTable.size() > 0;
+
             if(bootNodeRoutingTable.size() == 1){
                 //No other nodes so we do not findOtherNodes
-                return new Peer(id, myHost, myPort, bootNodeRoutingTable);
+                return new Peer(uuid, myHost, myPort, bootNodeRoutingTable);
             }
-            //findClosests
-            Pair<UUID, String> closest = findClosest(id, bootNodeRoutingTable);
+            //TODO create new routing table.
+            Map routingTable = new ConcurrentHashMap<>();
 
-            //create new routing table.
+            //findClosests neighbours
+            List<Pair<UUID, String>> closest = findClosest(uuid, bootNodeRoutingTable);
 
+            //take the last 4 peers and put them as our neighbours in routing table
+            try {
+                for (int i = closest.size(); i > closest.size()-4; i--) {
+                    routingTable.put(closest.get(i).getKey(), closest.get(i).getValue());
+                }
+            } catch (Exception e) {
+                System.out.println("did not find 4 neighbours this time...");
+            }
 
-            return new Peer(id, myHost, myPort, bootNodeRoutingTable);
+            return new Peer(uuid, myHost, myPort, routingTable);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
@@ -174,6 +229,7 @@ public class Peer {
             Socket s = new Socket(host_ip[0], Integer.parseInt(host_ip[1]));
             ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
 
+            //send message to peer asking for his closest peer to target
             out.write((CLOSEST_MSG+":"+target).getBytes("UTF-8"));
 
             ObjectInputStream in = new ObjectInputStream(s.getInputStream());
@@ -191,24 +247,36 @@ public class Peer {
         return null;
     }
 
-    public static Pair<UUID, String> findClosest(UUID target, Map<UUID, String> routingTable){
-        //find the closest peer in routing table
+    /*
+     * find the closest peer in routing table
+     */
+    public static List<Pair<UUID, String>> findClosest(UUID target, Map<UUID, String> routingTable){
+
+        //sort list
         UUID[] sorted = routingTable.keySet().toArray(new UUID[routingTable.size()]);
         Arrays.sort(sorted, (o1, o2) -> distance(target, o1)  - distance(target, o2));
+
         UUID closest = sorted[0], prevClosest = null;
 
         Pair<UUID, String> node = null;
 
+        List<Pair<UUID, String>> lastContacted = new ArrayList<>();
+
         while( closest != target && closest != prevClosest ){
             prevClosest = closest;
-            node = requestClosests(closest, routingTable.get(closest));
+            node = requestClosests(target, routingTable.get(closest));
+            lastContacted.add(node);
             if(node == null) return null;
             else closest = node.getKey();
         }
 
-        if(closest == target)
-            return node;
-        else return null;
+        return lastContacted;
+
+        //if(closest == target)
+        //    return node;
+        //else {
+        //
+        //};
 
 
     }
